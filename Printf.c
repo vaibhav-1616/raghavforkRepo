9508bc48f118e95bf4339de658067311419df342
Rave271 <271raghavverma@gmail.com>
2024-03-06 14:20:55 +0530

    addded files:

---
+++ Printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main(){
+    /*Practising Basic Stuff*/
+    printf("H");
+    printf("E");
+    printf("L");
+    printf("L");
+    printf("O");
+    printf("!!!!");
+    //return 0;
+}
---
+++ Variable-ASCII.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/*int main(){
+    char A;
+    printf("Input a Char:");
+    scanf("%c",&A);
+    printf("ASCII Value is :%c=%d",A,A);
+    return 0;
+}
+*/
+
+int main(){
+    int a,b,c;
+    printf("Input 2 Numbers");
+    scanf("%d %d",&a,&b);
+    c=a+b;
+    printf("SUM= %d",c);
+}
